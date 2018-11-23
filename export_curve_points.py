b5ea30ecc2d5d1e834ad4c960bc38d77ee4c5dcc
Baron Watts <watts.baron@hotmail.com>
2018-11-23 15:27:57 -0600

    Update export_curve_points.py

--- export_curve_points.py
+++ export_curve_points.py
@@ -15,6 +15,14 @@ if obj.type == 'CURVE':
 
             for nurbspoint in subcurve.points:
                 print( [nurbspoint.co[0], nurbspoint.co[1], nurbspoint.co[2]],',')
+                
+         
+        #Display poly curve coordinate points in an array
+        if curvetype == 'POLY':
+            print("curve is closed:", subcurve.use_cyclic_u)
+
+            for polypoint in subcurve.points:
+                print( [polypoint.co[0], polypoint.co[1], polypoint.co[2]],',')
 
 
         #Display bezier curve coordinate points in an array
